dae9a90e1cf99c61044d5af19222fae982c48259
Seyedamirhossein Hesamian <amir734jj@live.com>
2018-06-12 08:31:50 -0500

    Update task_model.py

--- logic/task_model.py
+++ logic/task_model.py
@@ -3,7 +3,7 @@ import uuid
 
 class Task:
     def __init__(self, name: str, script: str):
-        self.id = str(uuid.uuid4())
-        self.name = name
-        self.script = script
-        self.summery = script[:25]
+        self.id: str = str(uuid.uuid4())
+        self.name: str = name
+        self.script: str = script
+        self.summery: str = script[:25]
